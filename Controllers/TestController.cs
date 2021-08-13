019ac028469f3b61c4c50db5411b5b2b417d83d1
ksrydzyk <kuc.wojciech@gmail.com>
2021-08-13 21:32:26 +0200

    First commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,39 @@
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Text.Encodings.Web;
+using System;
+
+
+namespace MvcMovie.Controllers
+{
+    public class TestController : Controller
+    {
+        // 
+        // GET: /Test/
+        private TelemetryClient aiClient;
+
+        public TestController(TelemetryClient aiClient)
+        {
+            this.aiClient = aiClient;
+        }   
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/
+
+        public IActionResult Parameters(string name, int count)
+        {
+            this.aiClient.TrackEvent("Parameters invoked", new Dictionary<string, string> {{"Name", name}, {"Count", Convert.ToString(count)}});
+
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = count;
+
+            return View();
+        }
+    }
+}
